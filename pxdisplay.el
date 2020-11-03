a405969bb0c9d0c1118b4b41cb118bec3ff96ec3
nik <nik@nikhouri.com>
2020-11-03 14:10:26 +0000

    Fixed refresh key bindings

--- pxdisplay.el
+++ pxdisplay.el
@@ -182,8 +182,8 @@
 
 (progn
   (setq pxdisplay-mode-map (make-sparse-keymap))
-  (define-key pxdisplay-mode-map (kbd "r") 'pxdisplay-refresh)
-  (define-key pxdisplay-mode-map (kbd "R") 'pxdisplay-refresh)
+  (define-key pxdisplay-mode-map (kbd "r") 'pxdisplay)
+  (define-key pxdisplay-mode-map (kbd "R") 'pxdisplay)
   (define-key pxdisplay-mode-map (kbd "q") 'bury-buffer)
   (define-key pxdisplay-mode-map (kbd "Q") 'bury-buffer))
 
